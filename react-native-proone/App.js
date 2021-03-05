6d2893a7f5544dddb9169f469ea4282989788698
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-03-05 21:58:19 +0530

    Create App.js

---
+++ react-native-proone/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { SignUpScreen } from './screens';
+const Stack = createStackNavigator();
+import Tabs from './navigation/Tabs'
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border : 'transparent'
+  }
+}
+
+export class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions = {{headerShown : false}} 
+        initialRouteName = {SignUpScreen}
+        >
+        <Stack.Screen name = 'SignUp' component = {SignUpScreen}></Stack.Screen>
+        <Stack.Screen name = 'tabs' component = {Tabs}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
+}
+
+export default App
