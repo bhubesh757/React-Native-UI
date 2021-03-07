77107285b0d559b825ba6e638ecb55cbb220088c
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-03-07 21:58:29 +0530

    Create App.js

---
+++ Chefie-Food UI/App.js
@@ -0,0 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
+
+import {createStackNavigator} from '@react-navigation/stack'
+import BottomNavigator from './navigation/BottomNavigator';
+import DetailsScreen from './screens/DetailsScreen';
+import OnBoardScreen from './screens/OnBoardScreen';
+import COLORS from '/home/bhubesh/react-native-proone/consts/colors.js'
+const Stack = createStackNavigator();
+const App = () => {
+  return (
+   <NavigationContainer>
+     <StatusBar backgroundColor = 'transparent' barStyle = 'dark-content'></StatusBar>
+        <Stack.Navigator initialRouteName = 'Home' screenOptions = {{headerShown : false}} >
+          <Stack.Screen name = 'BoardScreen' component = {OnBoardScreen}></Stack.Screen>
+          <Stack.Screen name = 'Home' component = {BottomNavigator}></Stack.Screen>
+          <Stack.Screen name = 'DetailsScreen' component = {DetailsScreen}></Stack.Screen>
+        </Stack.Navigator>
+   </NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
