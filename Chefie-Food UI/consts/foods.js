f95292ab0d7dc07c9ff99938c7e02bd89f7bb586
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-03-08 15:08:02 +0530

    Add files via upload

--- Chefie-Food UI/consts/foods.js
+++ Chefie-Food UI/consts/foods.js
@@ -3,29 +3,33 @@ const foods = [
     id: '1',
     name: 'Meat Pizza',
     ingredients: 'Mixed Pizza',
-    price: '8.30',
+    price: '188',
     image: require('../assets/meatPizza.png'),
+    description : 'Black olives, capsicum, onion, grilled mushroom, corn, tomato, jalapeno & extra cheese Made of Freshly sourced American corn, bell peppers and onions, from a local ...'
   },
   {
     id: '2',
     name: 'Cheese Pizza',
     ingredients: 'Cheese Pizza',
-    price: '7.10',
+    price: '257',
     image: require('../assets/cheesePizza.png'),
+    description : 'Yearning for a cheesy delight? Then we have an easy cheesy recipe to add soul to your weekend binging! Weekend calls for some fun and nothing can be better than enjoying your favourite delight at home with your friends and family.'
   },
   {
     id: '3',
     name: 'Chicken Burger',
     ingredients: 'Fried Chicken',
-    price: '5.10',
+    price: '265',
     image: require('../assets/chickenBurger.png'),
+    description : 'description'
   },
   {
     id: '4',
     name: 'Sushi Makizushi',
     ingredients: 'Salmon Meat',
-    price: '9.55',
+    price: '319',
     image: require('../assets/sushiMakizushi.png'),
+    description : 'Makizushi is also known as rolled sushi or sushi rolls. This common type of sushi is made from seaweed and vinegared rice filled with different ingredients such as fish and vegetables.'
   },
 ];
 
