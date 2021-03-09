8c8d2ee441d2c68c1f90578d2d66e56d74a9d066
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-03-09 22:31:18 +0530

    Add files via upload

---
+++ Hotel UI/src/consts/hotels.js
@@ -0,0 +1,36 @@
+const hotels = [
+  {
+    id: '1',
+    name: 'Silver Hotel & SPA',
+    location: 'Green street,Central district',
+    price: 12000,
+    image: require('../../assets/hotel1.jpg'),
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+  },
+  {
+    id: '2',
+    name: 'Bring Hotel',
+    location: 'Yuki street',
+    price: 17070,
+    image: require('../../assets/hotel2.jpg'),
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+  },
+  {
+    id: '3',
+    name: 'Aluna Hotel',
+    location: 'Almond street',
+    price: 20190,
+    image: require('../../assets/hotel3.jpg'),
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+  },
+  {
+    id: '4',
+    name: 'Green Hotel',
+    location: 'Main street',
+    price: 32100,
+    image: require('../../assets/hotel4.jpg'),
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+  },
+];
+
+export default hotels;
